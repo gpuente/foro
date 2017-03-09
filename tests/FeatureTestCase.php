7eb07362a8cffa6e6a314f42f18a72103db412f2
Guillermo Puente <gpuente@borealis.cl>
2017-03-09 12:21:15 -0300

    Refactored method to validate fields with errors

--- tests/FeatureTestCase.php
+++ tests/FeatureTestCase.php
@@ -8,5 +8,26 @@
 
 		use DatabaseTransactions;
 
+		/*
+		/------------------------------------------------------------
+		/ Custom error validation
+		/------------------------------------------------------------
+		/
+		/ The following function validate if a field has error messages.
+		/ This validation only works with Styde html component.
+		/ For more information about this component, please refers to official documentation
+		/ https://github.com/StydeNet/html
+		/
+		*/
+		public function seeErrors(array $fields)
+		{
+			foreach ($fields as $name => $errors) {
+				foreach ((array) $errors as $message) {
+					$this->seeInElement("#field_{$name}.has-error .help-block", $message);
+				}
+			}
+			
+		}
+
 	}
 ?>
