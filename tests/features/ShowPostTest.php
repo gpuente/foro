4fa0555bff70c05acdf9a19e5298984fa327989a
Guillermo Puente <gpuente@borealis.cl>
2017-03-16 10:58:50 -0300

    Added redirect to pages with old slug

--- tests/features/ShowPostTest.php
+++ tests/features/ShowPostTest.php
@@ -27,9 +27,29 @@ class ShowPostTest extends FeatureTestCase
         $user->posts()->save($post);
 
         // When
-        $this->visit(route('posts.show', $post))
+        $this->visit($post->url)
             ->seeInElement('h1', $post->title)
             ->see($post->content)
             ->see($user->name);
+    }
+
+
+    public function test_old_urls_are_redirected()
+    {
+        // Having
+        $user = $this->defaultUser();
+
+        $post = factory(Post::class)->make([
+            'title'     => 'Old title',
+        ]);
+
+        $user->posts()->save($post);
+
+        $url = $post->url;
+
+        $post->update(['title' => 'New title']);
+
+        $this->visit($url)
+            ->seePageIs($post->url);
     }
 }
